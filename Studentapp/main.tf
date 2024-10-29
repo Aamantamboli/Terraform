3057da62257fc426e7bd39503eb5159717ca6e23
Aaman Tamboli <amantamboli671@gmail.com>
2024-10-29 16:20:27 +0530

    done

--- Studentapp/main.tf
+++ Studentapp/main.tf
@@ -1,1 +1,8 @@
+provider "aws" {
+  region = var.aws_region  # Change this to your desired region
+}
 
+resource "aws_instance" "student" {
+    ami = var.this_aws_ubuntu_ami_id
+    instance_type = var.this_aws_instance_instance_type
+}
--- Studentapp/variable.tf
+++ Studentapp/variable.tf
@@ -1,1 +1,15 @@
+variable "aws_region" {
+    type = string
+    default = "ap-south-1"
+}
+
+variable "this_aws_ubuntu_ami_id" {
+    type = string
+    description = "ami-0dee22c13ea7a9a67"
+}
+
+variable "this_aws_instance_instance_type" {
+       type = string
+       description= "t2.micro"
+}
 
