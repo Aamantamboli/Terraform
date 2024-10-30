aacb7146d67fed559577ff41997a933eb932f92c
Aaman Tamboli <amantamboli671@gmail.com>
2024-10-30 16:42:46 +0530

    Update main.tf

--- Studentapp/main.tf
+++ Studentapp/main.tf
@@ -2,6 +2,46 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_vpc" "studentvpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "studentsubnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
+resource "aws_security_group" "studentsecuritygroup" {
+  vpc_id = aws_vpc.studentvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
