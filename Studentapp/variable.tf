d0a208eb3afac366988ee8457475b8b544be6218
Aaman Tamboli <amantamboli671@gmail.com>
2024-10-30 16:25:12 +0530

    Update variable.tf

--- Studentapp/variable.tf
+++ Studentapp/variable.tf
@@ -4,6 +4,24 @@ variable "region" {
   default     = "ap-south-1"
 }
 
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_block" {
+  description = "The CIDR block for the subnet."
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet."
+  type        = string
+  default     = "us-east-1a"
+}
+
 variable "ami" {
   type = string
   description = "The AMI ID to use for the instance"
