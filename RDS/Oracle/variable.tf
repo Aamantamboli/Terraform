0612449149a93d6607d4068f364da4e7d8ac0c1b
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-07 16:53:08 +0530

    done

--- RDS/Oracle/variable.tf
+++ RDS/Oracle/variable.tf
@@ -1,69 +1,69 @@
-variable "aws_profile" {
+variable "thi_aws_profile" {
   description = "AWS profile to use"
   type        = string
   default     = "default"
 }
 
-variable "aws_region" {
+variable "thi_aws_region" {
   description = "AWS region for RDS instance"
   type        = string
   default     = "ap-south-1"
 }
 
-variable "db_identifier" {
+variable "this_db_identifier" {
   description = "RDS instance identifier"
   type        = string
   default     = "my-oracle-db"
 }
 
-variable "db_allocated_storage" {
+variable "this_db_allocated_storage" {
   description = "Storage size in GB"
   type        = number
   default     = 20
 }
 
-variable "db_instance_class" {
+variable "this_db_instance_class" {
   description = "DB instance class"
   type        = string
   default     = "db.t3.medium"
 }
 
-variable "db_name" {
+variable "this_db_name" {
   description = "Database name"
   type        = string
   default     = "mydb"
 }
 
-variable "db_username" {
+variable "this_db_username" {
   description = "Master username"
   type        = string
   default     = "admin"
 }
 
-variable "db_password" {
+variable "this_db_password" {
   description = "Master password"
   type        = string
 }
 
-variable "db_parameter_group" {
+variable "this_db_parameter_group" {
   description = "RDS parameter group for Oracle"
   type        = string
   default     = "default.oracle-se2-19"  # Adjust as needed based on the Oracle engine version
 }
 
-variable "publicly_accessible" {
+variable "this_publicly_accessible" {
   description = "If true, the RDS instance will be publicly accessible"
   type        = bool
   default     = false
 }
 
-variable "backup_retention_period" {
+variable "this_backup_retention_period" {
   description = "Number of days to retain backups"
   type        = number
   default     = 7
 }
 
-variable "multi_az" {
+variable "this_multi_az" {
   description = "If true, RDS instance will be deployed in multiple availability zones"
   type        = bool
   default     = false
