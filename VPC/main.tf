c3cb0434a33f9ea8a6542dc08c74a8b7beb162c3
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-07 12:45:32 +0530

    done

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,1 +1,71 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.this_vpc[0]
+  enable_dns_support = var.this_vpc[1]
+  enable_dns_hostnames = var.this_vpc[1]
+  tags = {
+    Name = var.this_vpc[2]
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = var.this_internet_gateway
+  }
+}
+
+# Create Public Subnet 1
+resource "aws_subnet" "my_subnet_1" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.this_subnet_1[0]
+  availability_zone       = var.this_subnet_1[1]
+  map_public_ip_on_launch = var.this_subnet_1[2]
+  tags = {
+    Name = var.this_subnet_1[3]
+  }
+}
+
+# Create Public Subnet 2
+resource "aws_subnet" "my_subnet_2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.this_subnet_2[0]
+  availability_zone       = var.this_subnet_2[1]
+  map_public_ip_on_launch = var.this_subnet_2[2]
+  tags = {
+    Name = var.this_subnet_2[3]
+  }
+}
+
+# Create Route Table for Public Subnets
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = var.this_route_table
+  }
+}
+
+# Create Route to Internet Gateway in the Route Table
+resource "aws_route" "my_route" {
+  route_table_id         = aws_route_table.my_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
+# Associate Route Table with Public Subnet 1
+resource "aws_route_table_association" "my_route_table_assoc_1" {
+  subnet_id      = aws_subnet.my_subnet_1.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Associate Route Table with Public Subnet 2
+resource "aws_route_table_association" "my_route_table_assoc_2" {
+  subnet_id      = aws_subnet.my_subnet_2.id
+  route_table_id = aws_route_table.my_route_table.id
+}
 
--- VPC/variable.tf
+++ VPC/variable.tf
@@ -1,1 +1,42 @@
+# AWS Region
+variable "aws_region" {
+  type        = string
+  default     = "ap-south-1"
+  description = "The variable is for aws region"
+}
+
+# VPC 
+variable "this_vpc" {
+  type        = list
+  default     = ["10.0.0.0/16" , "true" , "my-vpc"]
+  description = "The variable is for the VPC"
+}
+
+# Internet Gateway 
+variable "this_internet_gateway" {
+  type        = string
+  default     = "my-igw"
+  description = "The varaible is for Internte Gateway"
+}
+
+# Public Subnet 1 
+variable "this_subnet_1" {
+  type        = list
+  default     = ["10.0.1.0/24" , "ap-south-1a" , "true" , "subnet-1"]
+  description = "The variable is for the first public subnet"
+}
+
+# Public Subnet 2 CIDR block
+variable "this_subnet_2" {
+  type        = list
+  default     = ["10.0.2.0/24" , "ap-south-1b , "true" , "subnet-2"]
+  description = "The variable is for the second public subnet"
+}
+
+# Route Table Name
+variable "this_route_table" {
+  type        = string
+  default     = "my-route-table"
+  description = "The name of the route table"
+}
 
