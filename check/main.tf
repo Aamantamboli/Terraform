fed779b1f2d51d4cb75dcadcd53a605b404b4111
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-08 15:54:17 +0530

    done

--- check/main.tf
+++ check/main.tf
@@ -61,7 +61,7 @@ resource "aws_db_instance" "studentdb" {
   engine            = "mariadb"
   instance_class    = "db.t2.micro"
   allocated_storage = 20
-  name              = var.db_name
+  db_name           = var.db_name
   username          = var.db_username
   password          = var.db_password
   vpc_security_group_ids = [aws_security_group.studentsecuritygroup.id]
