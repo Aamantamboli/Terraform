a0d1412b3f56eacc43741000425412eda4c7bdd3
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-08 20:08:20 +0530

    done

--- check/rd.tf
+++ check/rd.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "student_db" {
   engine               = "mysql"
   engine_version       = "5.7.44-rds.20240808" # Keep the exact version
   instance_class       = "db.t3.micro"         # Switch to a compatible instance class
-  name                 = var.db_name
+  db_name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql5.7"
