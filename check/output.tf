206d030a06be241ff3ea12c8ea0410577f8aae34
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-08 20:02:51 +0530

    done

---
+++ check/output.tf
@@ -0,0 +1,7 @@
+output "rds_endpoint" {
+  value = aws_db_instance.student_db.endpoint
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
---
+++ check/rd.tf
@@ -0,0 +1,33 @@
+resource "aws_db_instance" "student_db" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+
+  # Configure security group for RDS
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
+
+resource "aws_security_group" "rds_sg" {
+  name = "rds_sg"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust to limit access if needed
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
