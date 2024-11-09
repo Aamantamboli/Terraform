11913852947b640acee4123606b3fe7103c01bf8
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-09 09:45:22 +0530

    done

--- check/rd.tf
+++ check/rd.tf
@@ -1,15 +1,16 @@
-resource "aws_db_instance" "mariadb_instance" {
-  identifier              = "my-mariadb-instance"  # Customize this name
+resource "aws_db_instance" "student_db" {
+  identifier              = "student"  # Customize this name
   engine                  = "mariadb"
   engine_version          = "10.5"                # Specify the desired MariaDB version
   instance_class          = "db.t3.micro"         # Choose an appropriate instance type
   allocated_storage       = 20                    # Storage size in GB
   storage_type            = "gp2"                 # General-purpose SSD storage
-  db_name                 = "mydatabase"          # Name of the database
-  username                = "admin"               # Master username
-  password                = "yourpassword"        # Master password (keep it secure)
+  db_name                    = var.db_name          # Name of the database
+  username                = var.db_username               # Master username
+  password                = var.db_password       # Master password (keep it secure)
   publicly_accessible     = false                 # Set to true if you want public access
   skip_final_snapshot     = true                  # Skips snapshot on deletion for testing
+
   # Configure security group for RDS
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 }
