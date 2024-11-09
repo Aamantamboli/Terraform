6293d42dadd2becc68f72c33d2a5b62d1d7a5a44
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-09 22:04:58 +0530

    done

--- Security Group/variable.tf
+++ Security Group/variable.tf
@@ -1,5 +1,23 @@
 variable "this_aws_security_group" {
     type = list 
     default = ["awssecuritygroup" , "22" , "80" , "tcp"]
-    description = "This variable is for aws security group"
+    description = "The variable is for aws security group"
+}
+
+variable "this_aws_security_group_name" {
+    type = string 
+    default = "examplesecuritygroup"
+    description = "The variable is for aws security group name"
+}
+
+variable "this_aws_security_group_port" {
+    type = list
+    default = ["22" , "80"]
+    description = "The variable is for aws security group port" 
+}
+
+variable "this_aws_security_group_protocol" {
+    type = string
+    default = "tcp"
+    description = "The variable is for aws security group protocol"
 }
