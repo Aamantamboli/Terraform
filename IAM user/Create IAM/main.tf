efbda3207c5cb08d3233ebc7116b54d71a9cea72
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-09 23:41:25 +0530

    done

--- IAM user/Create IAM/main.tf
+++ IAM user/Create IAM/main.tf
@@ -1,3 +1,4 @@
+#Create a user
 resource "aws_iam_user" "myname" {
   name = "myname"
   path = "/"
@@ -5,4 +6,32 @@ resource "aws_iam_user" "myname" {
   tags = {
     tag-key = "tag-value"
   }
+}
+
+#Create a policy
+resource "aws_iam_policy" "s3_read_only_policy" {
+  name        = "S3ReadOnlyPolicy"
+  description = "Policy for read-only access to a specific S3 bucket"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:ListBucket",
+          "s3:GetObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::my-bucket",          # Bucket-level permissions
+          "arn:aws:s3:::my-bucket/*"        # Object-level permissions within the bucket
+        ]
+      }
+    ]
+  })
+}
+
+#Attach a policy to user
+resource "aws_iam_user_policy_attachment" "attach_s3_policy" {
+  user       = aws_iam_user.my_user.name
+  policy_arn = aws_iam_policy.s3_read_only_policy.arn
 }
