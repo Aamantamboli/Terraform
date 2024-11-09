ef1da8d518e25b7f049e0b3de38de258a946e096
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-09 23:35:22 +0530

    done

--- IAM user/Create & Attach policy /mian.tf
+++ IAM user/Create & Attach policy /mian.tf
@@ -1,16 +1,16 @@
 resource "aws_iam_policy" "policy" {
   name        = "test-policy"
   description = "A test policy"
-  policy      = jsonencode{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*"
-        }
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "*"
+        Resource = "*"
+      }
     ]
-}
+  })
 }
 
 resource "aws_iam_user_policy_attachment" "test-attach" {
