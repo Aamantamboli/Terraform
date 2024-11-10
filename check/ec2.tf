b14a7fce18d83e9c142abfc0122b2eab115f70e3
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-10 12:36:26 +0530

    done

--- check/ec2.tf
+++ check/ec2.tf
@@ -63,3 +63,28 @@ resource "aws_instance" "app_server" {
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 }
+
+resource "aws_security_group" "ec2_sg" {
+  name = "ec2_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP access for Tomcat
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
