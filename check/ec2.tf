8ad0c4b81fa2e2378483742bde5ac31445df4030
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-10 12:57:36 +0530

    done

--- check/ec2.tf
+++ check/ec2.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "app_server" {
               sudo cp /home/ubuntu/Studentapp/*.jar /opt/tomcat/lib/
 
               # Start Tomcat
-              /opt/tomcat/bin/catalina.sh start
+              bash /opt/tomcat/bin/catalina.sh start
               EOF
 
   tags = {
