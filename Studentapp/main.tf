1ce4e10881a588d0dd355ab16c6335b107509463
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-13 17:51:48 +0530

    done

--- Studentapp/main.tf
+++ Studentapp/main.tf
@@ -61,11 +61,9 @@ resource "aws_instance" "studentapp" {
 
 user_data = <<-EOF
               #!/bin/bash
-              # Update the package repository
               sudo apt update -y
-
-              # Install Java and Maven
               sudo apt install openjdk-11-jre-headless -y maven
+              sudo apt install mariadb-client -y
 
               # Clone the repository
               git clone https://github.com/Aamantamboli/Studentapp.git /home/ubuntu/Studentapp
@@ -75,10 +73,10 @@ user_data = <<-EOF
               mvn clean package
 
               # Download and install Tomcat
-              cd /opt
-              curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz
-              tar -xvf apache-tomcat-9.0.97.tar.gz
-              sudo mv apache-tomcat-9.0.97 /opt/tomcat
+              cd /tmp
+              wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.96/bin/apache-tomcat-9.0.96.tar.gz
+              tar -xvf apache-tomcat-9.0.96.tar.gz
+              sudo mv apache-tomcat-9.0.96 /opt/tomcat
 
               # Set JAVA_HOME
               export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
@@ -86,8 +84,9 @@ user_data = <<-EOF
 
               # Copy the built artifacts to Tomcat's webapps directory
               sudo cp /home/ubuntu/Studentapp/target/*.war /opt/tomcat/webapps/
+              sudo cp /home/ubuntu/Studentapp/*.jar /opt/tomcat/lib/
 
               # Start Tomcat
-              sudo bash /opt/tomcat/bin/catalina.sh start
+              bash /opt/tomcat/bin/catalina.sh start
               EOF
 }
