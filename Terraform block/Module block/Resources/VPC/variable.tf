c02d33af7620d2041b81386c472cadee9c2fe1cd
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-18 11:26:09 +0530

    Update variable.tf

--- Terraform block/Module block/Resources/VPC/variable.tf
+++ Terraform block/Module block/Resources/VPC/variable.tf
@@ -1,1 +1,11 @@
+variable "this_vpc_cidr_block" {
+  description = "The variable is for CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
+variable "this_vpc_name" {
+  description = "The variable is for VPC name"
+  type        = string
+  default     = "example"
+}
