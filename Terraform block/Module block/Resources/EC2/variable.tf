63695736997da13ba2d9124e28fd3eee8cc845e0
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-18 11:21:15 +0530

    Update variable.tf

--- Terraform block/Module block/Resources/EC2/variable.tf
+++ Terraform block/Module block/Resources/EC2/variable.tf
@@ -1,1 +1,29 @@
+variable "this_ami" {
+  description = "The variable is for AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0dee22c13ea7a9a67"
+}
 
+variable "this_instance_type" {
+  description = "The variable is for EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "this_associate_public_ip" {
+  description = "Whether to associate a public IP with the EC2 instance"
+  type        = bool
+  default     = true
+}
+
+variable "this_instance_name" {
+  description = "The variable is for Name of the instance"
+  type        = string
+  default     = "StudentApp"
+}
+
+variable "this_key_name" {
+  description = "The variable is for Name of the SSH key pair"
+  type        = string
+  default     = "mynewaaman"
+}
