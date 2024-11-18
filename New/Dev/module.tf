c4b5f3a4d2f79d16a1afbc5aab14eea2f42eb7dc
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-18 17:38:36 +0530

    Update module.tf

--- New/Dev/module.tf
+++ New/Dev/module.tf
@@ -1,9 +1,9 @@
 module "aws_web_server_vpc" {
-  source = "./modules/aws-web-server-vpc"
+  source = "/opt/Terraform/New/Resources/Vpc"
 }
  
 module "aws_web_server_instance" {
-  source            = "./modules/aws-web-server-instance"
+  source            = "/opt/Terraform/New/Resources/Ec2"
   ec2_instance_type = var.ec2_instance_type
   vpc_id            = module.aws_web_server_vpc.vpc_id
   subnet_id         = module.aws_web_server_vpc.subnet_id
