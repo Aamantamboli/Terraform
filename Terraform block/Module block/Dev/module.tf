9bf5f911dc145fd4f8a0dff8e9fa5fccd3b04144
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-18 11:49:39 +0530

    done

--- Terraform block/Module block/Dev/module.tf
+++ Terraform block/Module block/Dev/module.tf
@@ -1,11 +1,11 @@
 module "vpc" {
-    source = "/opt/Terraform block/Module block/Resources/EC2"
+    source = "/opt/Terraform/Terraform block/Module block/Resources/VPC"
     this_vpc_cidr_block = "10.0.0.0/16"
     this_vpc_name = "example"
 }
 
 module "ec2" {
-    source = "/opt/Terraform block/Module block/Resources/VPC"
+    source = "/opt/Terraform/Terraform block/Module block/Resources/EC2"
     this_ami = "ami-0dee22c13ea7a9a67"
     this_instance_type = "t2.micro"
     this_associate_public_ip = true
