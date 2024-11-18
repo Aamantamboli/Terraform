d2d13955f3d4792cfc4795df1d796531b7c36c72
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-18 11:20:24 +0530

    Update main.tf

--- Terraform block/Module block/Resources/EC2/main.tf
+++ Terraform block/Module block/Resources/EC2/main.tf
@@ -1,1 +1,11 @@
-
+# EC2 Instance Creation
+resource "aws_instance" "example" {
+  ami                          = var.this_ami
+  instance_type                = var.this_instance_type
+  associate_public_ip_address  = var.this_associate_public_ip
+  vpc_security_group_ids       = [aws_security_group.studentsecuritygroup.id]  # Use vpc_security_group_ids
+  key_name                     = var.this_key_name 
+  tags = {
+    Name = var.this_instance_name
+  }
+}
