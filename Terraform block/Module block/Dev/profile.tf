a3e628248cf16cd859fddbc6df2a4de994735b45
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-18 11:43:51 +0530

    done

--- Terraform block/Module block/Dev/profile.tf
+++ Terraform block/Module block/Dev/profile.tf
@@ -1,1 +1,10 @@
-
+provider "aws" {
+        region = "uap-south-1"
+        profile = "configs"
+        default_tags {
+            tags = {
+              name = "aws"
+            }
+    
+        }
+}
