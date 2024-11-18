fa575f4136678eeddf638c95b234520dc46bc2b7
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-18 17:44:20 +0530

    Update module.tf

--- Terraform block/Module block/Dev/module.tf
+++ Terraform block/Module block/Dev/module.tf
@@ -18,8 +18,8 @@ module "ec2" {
     this_ami = "ami-0dee22c13ea7a9a67"
     this_instance_type = "t2.micro"
     this_associate_public_ip = true
-    #this_subnet_id = output.this_
-    #this_security_group = output.this_security_group_id
+    #this_subnet_id =  module.vpc.this_vpc_id
+    this_security_group =  module.vpc.this_security_group
     this_instance_name = "ExampleInstance"
     this_key_name ="mynewaaman"
 }
