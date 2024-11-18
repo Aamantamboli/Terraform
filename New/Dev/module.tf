da142358268142608100df06a27dcb4de293235c
Aaman Tamboli <amantamboli671@gmail.com>
2024-11-18 17:30:32 +0530

    Update module.tf

--- New/Dev/module.tf
+++ New/Dev/module.tf
@@ -1,1 +1,10 @@
-
+module "aws_web_server_vpc" {
+  source = "./modules/aws-web-server-vpc"
+}
+ 
+module "aws_web_server_instance" {
+  source            = "./modules/aws-web-server-instance"
+  ec2_instance_type = var.ec2_instance_type
+  vpc_id            = module.aws_web_server_vpc.vpc_id
+  subnet_id         = module.aws_web_server_vpc.subnet_id
+}
