166bd3080a42daf4390ee337abd4177d5d5b2225
Aaman Tamboli <amantamboli671@gmail.com>
2025-01-03 23:29:43 +0530

    Update main.tf

--- Workspace/main.tf
+++ Workspace/main.tf
@@ -7,11 +7,18 @@ variable "ami" {
 }
 
 variable "instance_type" {
- description = "value" 
+  description = "value"
+  type = map(string)
+
+  default = {
+    "dev" = "t2.micro"
+    "stage" = "t2.medium"
+    "prod" = "t2.xlarge"
+  }
 }
 
 module "ec2_instance" {
-  source = "./module/ec2_instance"
+  source = "./modules/ec2_instance"
   ami = var.ami
-  instance_type = var.instance_type
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
 }
